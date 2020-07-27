c1b4d0b72987da13dbc9105df4cc44cc3058f6c1
daydaylee <1243437764@qq.com>
2020-07-27 14:36:08 +0800

    '完成vue项目的说明'

---
+++ demos/scroll.vue
@@ -0,0 +1,110 @@
+<template>
+  <div ref="wrapper">
+    <slot></slot>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+  export default {
+    props: {
+      probeType: {
+        type: Number,
+        default: 1
+      },
+      click: {
+        type: Boolean,
+        default: true
+      },
+      listenScroll: {
+        type: Boolean,
+        default: false
+      },
+      data: {
+        type: Array,
+        default: null
+      },
+      pullup: {
+        type: Boolean,
+        default: false
+      },
+      beforeScroll: {
+        type: Boolean,
+        default: false
+      },
+      refreshDelay: {
+        type: Number,
+        default: 20
+      }
+    },
+    mounted() {
+      setTimeout(() => {
+        this._initScroll()
+      }, 20)
+    },
+    methods: {
+      // 初始化Scroll
+      _initScroll() {
+        // 判断是否初始化
+        if (!this.$refs.wrapper) {
+          return
+        }
+        // 调用Scroll实例,表现可以滑动
+        this.scroll = new BScroll(this.$refs.wrapper, {
+          probeType: this.probeType,
+          click: this.click
+        })
+        // 判断是否需要监听滚动事件,需要的话，调用scroll实例上面的方法
+        if (this.listenScroll) {
+          let me = this
+          this.scroll.on('scroll', (pos) => {
+            // me指的就是实例
+            // 通过监听scroll事件,有一个回调,pos是一个对象,有x,y轴的具体距离
+            // 去派发一个scroll事件,这样子外部也就是父组件可以拿到我们的pos
+            me.$emit('scroll', pos)
+          })
+        }
+        if (this.pullup) {
+          this.scroll.on('scrollEnd', () => {
+            if (this.scroll.y <= (this.scroll.maxScrollY + 50)) {
+              this.$emit('scrollToEnd')
+            }
+          })
+        }
+        if (this.beforeScroll) {
+          this.scroll.on('beforeScrollStart', () => {
+            this.$emit('beforeScroll')
+          })
+        }
+      },
+      disable() {
+        this.scroll && this.scroll.disable()
+      },
+      enable() {
+        this.scroll && this.scroll.enable()
+      },
+      refresh() {  // 刷新scroll,重新计算高度
+        this.scroll && this.scroll.refresh()
+      },
+      scrollTo() {
+        // 代理better-scroll的scrollTo方法
+        this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+      },
+      scrollToElement() {
+        // 代理better-scroll的scrollToElement方法
+        this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
+      }
+    },
+    watch: {
+      // 监听到数据的变化,就会重新去refresh数据,重新去计算响应的数据
+      data() {
+        setTimeout(() => {
+          this.refresh()
+        }, this.refreshDelay)
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
