fc9c054a85aa8d84337c5b42e7e74c1a3d557b52
daydaylee <1243437764@qq.com>
2020-08-03 10:30:54 +0800

    更新快速排序错误问题

--- 算法/排序算法/快速排序.js
+++ 算法/排序算法/快速排序.js
@@ -21,8 +21,6 @@ let quickSort = function (arr) {
     let pivot = arr.splice(index, 1)[0],
         left = [],
         right = [];
-    console.log(pivot)
-    console.log(arr)
     for (let i = 0; i < arr.length; i++) {
         if (pivot > arr[i]) {
             left.push(arr[i])
