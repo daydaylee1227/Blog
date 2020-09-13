fa9cbd79573b27f4ca515be670c95c603e6586a7
daydaylee <1243437764@qq.com>
2020-09-13 20:28:01 +0800

    状态规划3

---
+++ 算法/动态规划/leetcode-买卖股票最大时机.js
@@ -0,0 +1,55 @@
+/**
+ * @param {number} k
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+ // 第一步:定义状态
+ // 可以定义如下
+ // dp[i][j][0] 表示第i天交易了j次时卖出后的累计最大利润
+ //  dp[i][j][1] 表示第i天交易了j次时买入后的累计最大利润
+
+
+
+ // 第二步：确定状态转移方程
+ // dp[i][j][0] 当第i天不持股的话，我们需要确定昨天是否持有股票
+ // dp[i][j][0] = max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i])
+ // dp[i][j][1] 同样的第i天,我们需要去确定昨天是否持有股票
+ // dp[i][j][1] = max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i])
+
+
+ // 初始化状态
+
+ var maxProfit = function(k, prices) {
+    let len = prices.length;
+    if(len === 0) return 0;
+    if(k >= len/2) return maxProfit2(prices);
+    let dp = Array.from(new Array(len), () => new Array(k+1));
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j <= k; j++) {
+            dp[i][j] = new Array(2).fill(0);
+        }
+    }
+    for (let i = 0; i < len; i++) {
+        for (let j = k; j > 0; j--) {
+            if(i===0) {
+                dp[i][j][0] = 0;
+                dp[i][j][1] = -prices[i];
+                continue;
+            }
+            dp[i][j][0] = Math.max(dp[i-1][j][0], dp[i-1][j][1] + prices[i]);
+            dp[i][j][1] = Math.max(dp[i-1][j][1], dp[i-1][j-1][0] - prices[i]);
+        }
+    }
+    return dp[len-1][k][0];
+};
+
+function maxProfit2(prices) {
+    let profits = 0;
+    for (let i = 0; i < prices.length + 1; i++) {
+        if(prices[i + 1] - prices[i] > 0) {
+            profits += prices[i + 1] - prices[i]
+        }
+    }
+    return profits;
+};
