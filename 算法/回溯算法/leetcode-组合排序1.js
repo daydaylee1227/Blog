3de4b2a4e913f159d2369272ba7cab2c072396eb
daydaylee1227 <1243437764@qq.com>
2021-01-09 11:00:34 +0800

    组合总和1 题解

---
+++ 算法/回溯算法/leetcode-组合排序1.js
@@ -0,0 +1,32 @@
+
+// https://leetcode-cn.com/problems/combination-sum/
+// 输入：candidates = [2,3,5], target = 8,
+// 所求解集为：
+// [
+//   [2,2,2,2],
+//   [2,3,3],
+//   [3,5]
+// ]
+
+
+combinationSum = (candidates, target) => {
+    const result = []
+    const dfs = (list, sum, index) => {
+      // 减去枝叶
+      if (sum > target) return 
+      if (sum === target) {
+        console.log('lsit === ', list);
+        result.push([...list])
+        return 
+      }
+      candidates.forEach((el, i) => {
+        if (i >= index) { // index做数组判断
+          list.push(el)
+          dfs(list, sum + el, i)
+          list.pop() 
+        }
+      })
+    }
+    dfs([], 0, 0)
+    return result
+  }
